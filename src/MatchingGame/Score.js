5774576b65b34ce10afc7383546eafbb470835e4
Wilson Chen <wilson.chen@codeimmersives.com>
2020-10-21 13:03:10 -0400

    update

---
+++ src/MatchingGame/Score.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Score extends Component {
+    render() {
+        const {valueArray} = this.props
+        const totalScore = valueArray.length
+        return (
+            <div className='score'>
+                
+            </div>
+        )
+    }
+}
+
