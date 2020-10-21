cb3579c36494b522d73273735c9c0a78fbfb55c9
Steven Lorenzo <stevenlorenzo@Stevens-MBP.attlocal.net>
2020-10-21 11:12:40 -0400

    modularized and refactored code

---
+++ src/MatchingGame/GridItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class GridItem extends React.Component{
+  constructor(props){
+      super(props)
+      this.state = {
+          isFlipped: false,
+      }
+  }
+  render(){
+      const {isFlipped} = this.state
+      const {value} = this.props
+      return (
+          <div
+              className={isFlipped ? 'grid-item flipped' : 'grid-item'}
+              onClick={()=>{
+                  this.setState({isFlipped: !isFlipped})
+              }}>
+              <div className='value'>{value}</div>
+          </div>
+      )
+  }
+}
+
+export default GridItem
