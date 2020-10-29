8df76d6318f25e0ce481d51aa11838a2c3375173
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-10-29 08:55:01 -0700

    pull

--- api/routes/users/model/User.js
+++ api/routes/users/model/User.js
@@ -3,33 +3,36 @@ const moment = require('moment');
 const now = moment();
 
 var UserSchema = new mongoose.Schema({
-    firstName: {
-        type: String,
-        trim: true,
-        required: true,
-        default: ''
-    },
-    lastName: {
-        type: String,
-        trim: true,
-        required: true,
-        default: ''
-    },
-    phone: {
-        type: String,
-        trim: true,
-        required: true,
-        default: ''
-    },
-    email: {
-        type: String,
-        trim: true,
-        unique: true,
-        require: true,
-        default: ''
-    },
-    password: { type: String, default: ''},
-    timestamp: {type: String, default: now.format('dddd, MMMM Do YYYY, h:mm:ss a')}
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+    default: '',
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    required: true,
+    default: '',
+  },
+  phone: {
+    type: String,
+    trim: true,
+    required: true,
+    default: '',
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    require: true,
+    default: '',
+  },
+  password: { type: String, default: '' },
+  timestamp: {
+    type: String,
+    default: now.format('dddd, MMMM Do YYYY, h:mm:ss a'),
+  },
 });
 
 module.exports = mongoose.model('users', UserSchema);
--- api/routes/users/users.js
+++ api/routes/users/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var userController = require('./controller/userController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
