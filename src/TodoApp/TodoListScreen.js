62024064c48063a9404f41d7aca3102a7ba79502
Wilson Chen <wilson.chen@codeimmersives.com>
2020-11-04 12:22:21 -0500

    deleteItem and showTodoModal

--- src/TodoApp/TodoListScreen.js
+++ src/TodoApp/TodoListScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Container, Button, AppBar, Toolbar, Avatar, Checkbox, TextField} from '@material-ui/core';
+import {
+  Container,
+  Button,
+  AppBar,
+  Toolbar,
+  Avatar,
+  Checkbox,
+  TextField,
+} from '@material-ui/core';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import SettingsIcon from '@material-ui/icons/Settings';
@@ -13,245 +21,235 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import {fetchTodoItems, saveTodoItem} from '../reducers/TodoSlice'
-import {logout} from '../reducers/AuthSlice'
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {
+  fetchTodoItems,
+  saveTodoItem,
+  deleteTodoItem,
+  setTodoModal,
+} from '../reducers/TodoSlice';
+import { logout } from '../reducers/AuthSlice';
 
+class TodoModal extends React.Component {
+  render() {
+    const { open, handleClose, handleSave, item, updateItem } = this.props;
 
-class TodoModal extends React.Component{
-
-    render(){
-        const {open, handleClose, handleSave, item, updateItem} = this.props
-
-        return item ? (
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle id="form-dialog-title">Todo</DialogTitle>
-                <DialogContent>
-                      <TextField
-                        className='modal-field'
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Description"
-                        value={item.description}
-                        onChange={(e)=>updateItem(e.target.value)}
-                        fullWidth
-                      />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">Cancel</Button>
-                    <Button onClick={handleSave} color="primary">Save</Button>
-                </DialogActions>
-              </Dialog>
-         ): null;
-    }
-}
-
-
-class TodoItem extends React.Component{
-    render(){
-        const {item, editItem, deleteItem, completeItem} = this.props
-
-        return (
-            <div className={item.isComplete ? 'todo-item completed' : 'todo-item'}>
-                <div className='left'>
-                    <Checkbox/>
-                    <div className='description'>{item.description}</div>
-                </div>
-                <div className='right'>
-                    <div className='complete-btn' onClick={completeItem}>Completed</div>
-                    <EditIcon onClick={editItem}/>
-                    <HighlightOffIcon onClick={deleteItem}/>
-                </div>
-            </div>
-        )
-    }
-}
-
-
-class TodoListScreen extends React.Component{
-
-    constructor(props){
-        super(props)
-        this.state = {
-            showTodoModal:false,
-            modalItem: null,
-        }
-    }
-
-    componentDidMount(){
-        this.props.fetchTodoItems()
-    }
-
-    deleteItem = async (itemID) => {
-        const {token} = this.props
-        const deleteUrl = 'http://localhost:9000/api/todos/'
-        const response = await fetch(deleteUrl, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({_id:itemID})
-        });
-
-        const todoList = this.state.todoList.filter((item)=>item._id!=itemID)
-        this.setState({todoList})
-    }
-
-    completeItem = async (item) => {
-        const {token} = this.props
-        const updatedItem = Object.assign({}, item, {isComplete: true})
-        const updateUrl = 'http://localhost:9000/api/todos/'
-        const response = await fetch(updateUrl, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(updatedItem)
-        });
-
-        const todoList = this.state.todoList.map((i)=>{
-            if( i._id == updatedItem._id ){
-                return updatedItem
-            }
-
-            return i
-        })
-        this.setState({todoList})
-    }
-
-    render(){
-        const todoItems = this.props.todoList.map((item)=><TodoItem
-            key={item._id}
-            item={item}
-            editItem={()=>{
-                this.setState({modalItem: item, showTodoModal: true})
-            }}
-            deleteItem={()=>this.deleteItem(item._id)}
-            completeItem={()=>this.completeItem(item)}
-        />)
-        const {showTodoModal, modalItem} = this.state
-        return (
-            <Container className='todo-dashboard'>
-
-               {/*  Modals */}
-               <TodoModal
-                  open={showTodoModal}
-                  item={modalItem}
-                  handleClose={()=>{
-                    this.setState({showTodoModal: false})
-                  }}
-                  handleSave={() => {
-                    this.props.saveTodoItem(modalItem)
-                    this.setState({showTodoModal: false})
-                  }}
-                  updateItem={(description)=>{
-                    const updatedModalItem = Object.assign({}, modalItem, {description})
-                    this.setState({modalItem: updatedModalItem})
-                  }}
-               />
-
-               {/* App Bar */}
-               <AppBar position="static">
-
-                  <Toolbar className='tool-bar'>
-
-                    {/* App Title */}
-                    <div className='app-title'>
-                        <ListAltIcon />
-                        <div className='name'>Todo App</div>
-                    </div>
-
-                    {/* Search Bar */}
-                    <div className='search-bar'>
-                        <SearchIcon/>
-                        <input type='text'/>
-                    </div>
-
-                    {/* Super Nav */}
-                    <div className='super-nav'>
-
-                        <div className='toggle'>
-                            <Avatar className='avatar'>S</Avatar>
-                            <div className='username'>Steven L.</div>
-                            <KeyboardArrowDownIcon className='chevron'/>
-                        </div>
-
-                        <div className='super-dd-menu'>
-                            <div className='dd-item'>
-                                <SettingsIcon/>
-                                <label>Settings</label>
-                            </div>
-                            <div className='dd-item' onClick={this.props.logout}>
-                                <ExitToAppIcon/>
-                                <label>Logout</label>
-                            </div>
-                        </div>
-
-                    </div>
-
-                  </Toolbar>
-
-                </AppBar>
-
-                {/* List Container */}
-                <div className='list-container'>
-
-                    <div className='todo-list'>
-                        {todoItems}
-                    </div>
-
-                    <div className='control-bar'>
-
-                        <div className='left'>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                className='btn create-btn'
-                                onClick={()=>{
-                                    const modalItem = {
-                                        _id: null,
-                                        description: '',
-                                        isComplete: false,
-                                    }
-                                    this.setState({showTodoModal: true, modalItem})
-                                }}
-                              >New Todo</Button>
-                        </div>
-
-                        <div className='right'>
-                            <Button variant="contained" color="primary" className='btn'>Complete</Button>
-                            <Button variant="contained" color="secondary" className='btn'>Delete</Button>
-                        </div>
-
-                    </div>
-
-                </div>
-
-            </Container>
-        )
-    }
-}
-
-
-const mapStateToProps = (state) => {
-  return {
-    token : state.auth.token,
-    todoList: state.todo.todoList,
+    return item ? (
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle id="form-dialog-title">Todo</DialogTitle>
+        <DialogContent>
+          <TextField
+            className="modal-field"
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Description"
+            value={item.description}
+            onChange={(e) => updateItem(e.target.value)}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleSave} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    ) : null;
   }
 }
 
-const mapDispatchToProps = (dispatch) => bindActionCreators({
-    fetchTodoItems,
-    saveTodoItem,
-    logout,
-}, dispatch)
+class TodoItem extends React.Component {
+  render() {
+    const { item, editItem, deleteItem, completeItem } = this.props;
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoListScreen)
+    return (
+      <div className={item.isComplete ? 'todo-item completed' : 'todo-item'}>
+        <div className="left">
+          <Checkbox />
+          <div className="description">{item.description}</div>
+        </div>
+        <div className="right">
+          <div className="complete-btn" onClick={completeItem}>
+            Completed
+          </div>
+          <EditIcon onClick={editItem} />
+          <HighlightOffIcon onClick={deleteItem} />
+        </div>
+      </div>
+    );
+  }
+}
 
+class TodoListScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalItem: null,
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchTodoItems();
+  }
+
+  completeItem = async (item) => {
+    const { token } = this.props;
+    const updatedItem = Object.assign({}, item, { isComplete: true });
+    const updateUrl = 'http://localhost:9000/api/todos/';
+    const response = await fetch(updateUrl, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(updatedItem),
+    });
+
+    const todoList = this.state.todoList.map((i) => {
+      if (i._id == updatedItem._id) {
+        return updatedItem;
+      }
+
+      return i;
+    });
+    this.setState({ todoList });
+  };
+
+  render() {
+    const todoItems = this.props.todoList.map((item) => (
+      <TodoItem
+        key={item._id}
+        item={item}
+        editItem={() => {
+          this.props.setTodoModal(true);
+          this.setState({ modalItem: item });
+        }}
+        deleteItem={() => this.props.deleteTodoItem(item._id)}
+        completeItem={() => this.completeItem(item)}
+      />
+    ));
+    const { modalItem } = this.state;
+    const { showTodoModal } = this.props;
+    return (
+      <Container className="todo-dashboard">
+        {/*  Modals */}
+        <TodoModal
+          open={showTodoModal}
+          item={modalItem}
+          handleClose={() => {
+            this.props.setTodoModal(false);
+          }}
+          handleSave={() => {
+            this.props.saveTodoItem(modalItem);
+            this.props.setTodoModal(false);
+          }}
+          updateItem={(description) => {
+            const updatedModalItem = Object.assign({}, modalItem, {
+              description,
+            });
+            this.setState({ modalItem: updatedModalItem });
+          }}
+        />
+
+        {/* App Bar */}
+        <AppBar position="static">
+          <Toolbar className="tool-bar">
+            {/* App Title */}
+            <div className="app-title">
+              <ListAltIcon />
+              <div className="name">Todo App</div>
+            </div>
+
+            {/* Search Bar */}
+            <div className="search-bar">
+              <SearchIcon />
+              <input type="text" />
+            </div>
+
+            {/* Super Nav */}
+            <div className="super-nav">
+              <div className="toggle">
+                <Avatar className="avatar">S</Avatar>
+                <div className="username">Steven L.</div>
+                <KeyboardArrowDownIcon className="chevron" />
+              </div>
+
+              <div className="super-dd-menu">
+                <div className="dd-item">
+                  <SettingsIcon />
+                  <label>Settings</label>
+                </div>
+                <div className="dd-item" onClick={this.props.logout}>
+                  <ExitToAppIcon />
+                  <label>Logout</label>
+                </div>
+              </div>
+            </div>
+          </Toolbar>
+        </AppBar>
+
+        {/* List Container */}
+        <div className="list-container">
+          <div className="todo-list">{todoItems}</div>
+
+          <div className="control-bar">
+            <div className="left">
+              <Button
+                variant="contained"
+                color="primary"
+                className="btn create-btn"
+                onClick={() => {
+                  const modalItem = {
+                    _id: null,
+                    description: '',
+                    isComplete: false,
+                  };
+                  this.props.setTodoModal(true);
+                  this.setState({ modalItem });
+                }}
+              >
+                New Todo
+              </Button>
+            </div>
+
+            <div className="right">
+              <Button variant="contained" color="primary" className="btn">
+                Complete
+              </Button>
+              <Button variant="contained" color="secondary" className="btn">
+                Delete
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Container>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    token: state.auth.token,
+    todoList: state.todo.todoList,
+    showTodoModal: state.todo.showTodoModal,
+  };
+};
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      fetchTodoItems,
+      saveTodoItem,
+      deleteTodoItem,
+      logout,
+      setTodoModal,
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoListScreen);
--- src/reducers/TodoSlice.js
+++ src/reducers/TodoSlice.js
@@ -1,79 +1,99 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 // Async Functions
 export const fetchTodoItems = createAsyncThunk(
   'fetchTodoItems',
   async (args, thunkAPI) => {
-    const token = thunkAPI.getState().auth.token
-    const fetchUrl = 'http://localhost:9000/api/todos/'
+    const token = thunkAPI.getState().auth.token;
+    const fetchUrl = 'http://localhost:9000/api/todos/';
     const response = await fetch(fetchUrl, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
     });
-    let todoList = await response.json()
-    return todoList
+    let todoList = await response.json();
+    return todoList;
   }
-)
-
+);
 
 export const saveTodoItem = createAsyncThunk(
   'saveTodoItem',
   async (todoItem, thunkAPI) => {
-    console.log('Todo Item: ', todoItem)
-    const token = thunkAPI.getState().auth.token
-    const createUrl = 'http://localhost:9000/api/todos/'
+    console.log('Todo Item: ', todoItem);
+    const token = thunkAPI.getState().auth.token;
+    const createUrl = 'http://localhost:9000/api/todos/';
     const response = await fetch(createUrl, {
-        method: todoItem._id ? 'PATCH' : 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(todoItem)
+      method: todoItem._id ? 'PATCH' : 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(todoItem),
     });
 
-    let todoList = []
-    if( todoItem._id ){
-        todoList = thunkAPI.getState().todo.todoList.map((item) => {
-            if( item._id == todoItem._id ){
-                return todoItem
-            }
-            return item
-        })
-    }else{
-        let newTodoItem = await response.json()
-        todoList = thunkAPI.getState().todo.todoList.concat(newTodoItem)
+    let todoList = [];
+    if (todoItem._id) {
+      todoList = thunkAPI.getState().todo.todoList.map((item) => {
+        if (item._id == todoItem._id) {
+          return todoItem;
+        }
+        return item;
+      });
+    } else {
+      let newTodoItem = await response.json();
+      todoList = thunkAPI.getState().todo.todoList.concat(newTodoItem);
     }
 
-    return todoList
+    return todoList;
   }
+);
+export const deleteTodoItem = createAsyncThunk(
+  'deleteTodoItem',
+  async (itemID, thunkAPI) => {
+    const token = thunkAPI.getState().auth.token;
+    const deleteUrl = 'http://localhost:9000/api/todos/';
+    const response = await fetch(deleteUrl, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ _id: itemID }),
+    });
+    const todoList = thunkAPI
+      .getState()
+      .todo.todoList.filter((item) => item._id != itemID);
 
-)
-
+    return todoList;
+  }
+);
 
 //
 export const todoSlice = createSlice({
   name: 'todo',
   initialState: {
     todoList: [],
+    showTodoModal: false,
   },
 
   reducers: {
-    setToken: (state, { payload }) => {
-       state.token = payload.token
+    setTodoModal: (state, action) => {
+      state.showTodoModal = action.payload;
     },
   },
 
   extraReducers: {
     [fetchTodoItems.fulfilled]: (state, action) => {
-        state.todoList = action.payload
+      state.todoList = action.payload;
     },
     [saveTodoItem.fulfilled]: (state, action) => {
-        state.todoList = action.payload
+      state.todoList = action.payload;
     },
-  }
+    [deleteTodoItem.fulfilled]: (state, action) => {
+      state.todoList = action.payload;
+    },
+  },
+});
 
-})
-
-export const { setToken } = todoSlice.actions
+export const { setTodoModal } = todoSlice.actions;
