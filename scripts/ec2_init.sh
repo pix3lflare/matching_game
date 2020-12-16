5c096b81a0fa8d02bcbffce51b92f2aa227d76a4
Steven Lorenzo <stevenlorenzo@Stevens-MBP.attlocal.net>
2020-12-16 15:37:41 -0500

    deploy test #8

--- scripts/ec2_init.sh
+++ scripts/ec2_init.sh
@@ -1,6 +1,8 @@
+cd /home/ec2-user
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+. /.nvm/nvm.sh
+
 nvm install node
 
-cd /home/ec2-user
 npm install
