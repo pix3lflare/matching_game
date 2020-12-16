8225b7600dc5513a6234393e8158f41592e73334
Steven Lorenzo <stevenlorenzo@Stevens-MacBook-Pro.local>
2020-12-16 17:48:17 -0500

    testing build

---
+++ build/precache-manifest.fc794f163a42489c20e94152c684a9aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b234e0ee4ef8200c64b859f476b113f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2145350adfeb6ba1e51",
+    "url": "/static/css/main.31c530d2.chunk.css"
+  },
+  {
+    "revision": "e845b14cb1eedd00d160",
+    "url": "/static/js/2.f7bc4421.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.f7bc4421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2145350adfeb6ba1e51",
+    "url": "/static/js/main.07f252a2.chunk.js"
+  },
+  {
+    "revision": "a7da85599b9fa85f1949",
+    "url": "/static/js/runtime-main.5a3fbe82.js"
+  }
+]);
